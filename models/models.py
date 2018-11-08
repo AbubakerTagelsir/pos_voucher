0a68b7b559912006e19f29a515512aa9692596e3
Baraah <baraahadil369@outlook.com>
2018-11-08 11:11:41 +0200

    add StockValue model

--- models/models.py
+++ models/models.py
@@ -10,6 +10,8 @@ class pos_voucher(models.Model):
 	phonenumber = fields.Char()
 	stock_lines = fields.One2many('stock.line', 'pos_id', "Lines")
 	trans_history = fields.One2many('pos.trans', 'pos_id')
+	company = fields.One2many('stock.value','pos_id')
+
 
 	@api.onchange('stock_lines')
 	def _get_stock_count(self):
@@ -30,6 +32,46 @@ class pos_voucher(models.Model):
 					total_value -= line.qty
 			p.balance = total_value
 
+
+	@api.onchange('stock_lines')
+	def _get_company_stock(self):
+		comp = self.env['company.company'].search([])
+		values = {}
+		for c in comp:
+			comp_total = 0
+			for sl in self.stock_lines:
+				if sl.company_id.id == c.id:
+					comp_total += sl.qty * sl.card_id.value
+			values[c.id] = comp_total
+		for line in self.company:
+			line.stock = values[line.company_id.id]
+
+	# @api.onchange('stock_lines','company')
+	# def _get_company_stock(self):
+	# 	companies = set()
+	# 	for record in self:
+	# 		companies.add(record.stock_lines.company_id.name)
+		
+	# 	# for p in self.stock_lines:
+	# 	# 	if p.company_id not in companies:
+	# 	# 		companies.append(p.company_id)
+
+	# 	comp_balance = []
+	# 	for c in companies:
+	# 		comp_total = 
+	# 		for l in self.stock_lines:
+	# 			if l.company_id == c:
+	# 				comp_total+= l.qty * l.card_id.value
+
+	# 		comp_balance.append(comp_total)
+	# 	return comp_balance
+			
+
+
+
+
+		                       
+
 class card(models.Model):
 	_name = 'card.card'
 	name = fields.Char()
@@ -41,13 +83,27 @@ class company(models.Model):
 	_name = 'company.company'
 	name = fields.Char()
 	imag = fields.Binary()
+	stock_lines = fields.One2many('stock.line', 'company_id')
+	
 
+	
+	
+
+class StockValue(models.Model):
+	_name = 'stock.value'
+	pos_id = fields.Many2one('pos_voucher.pos_voucher')
+	company_id = fields.Many2one('company.company')
+	stock = fields.Float()	
+
+
+	
 
 class StockLine(models.Model):
 	_name = 'stock.line'
 	card_id = fields.Many2one('card.card')
 	qty = fields.Integer("Quantity")
 	pos_id = fields.Many2one('pos_voucher.pos_voucher')
+	company_id = fields.Many2one('company.company')
 
 
 class Transaction(models.Model):
@@ -55,3 +111,4 @@ class Transaction(models.Model):
 	trans_type = fields.Selection([('in', "IN"), ('out', "OUT")], default='out', string="Transaction Type")
 	pos_id = fields.Many2one('pos_voucher.pos_voucher', "POS")
 	qty = fields.Integer("Quantity")
+
