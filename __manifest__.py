f60f9411e65ee15f9b4ffaa84fee00dcf58e5b90
Moataman <motaman1996@hotmail.com>
2018-11-14 08:05:10 +0200

    Qweb report- not working

--- __manifest__.py
+++ __manifest__.py
@@ -26,6 +26,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'reports/pos_voucher_report.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
